184d1c48702b687dc1cb5b16f437f5667acfec65
joyal m a <joyalaugustine12@gmail.com>
2026-01-16 22:13:56 +0530

    started creating player.jsx file

--- src/Pages/Player/Player.jsx
+++ src/Pages/Player/Player.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
 
 const Player = () => {
   return (
     <div className='player'>
-      
+      <img src={back_arrow_icon} alt="" />
+      <iframe width='90%' height='90%' src='https://www.youtube.com/embed/wb49-oV0F78' title='trailer' frameBorder='0' allowFullScreen></iframe>
     </div>
   )
 }
