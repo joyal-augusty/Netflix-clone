5f18e91114e1d4e407475d614865c6c56165ebe0
joyal m a <joyalaugustine12@gmail.com>
2026-01-19 20:44:20 +0530

    create firebase logic

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAFsDSMpnudOmG-xY9EKkRrvCYuD5u0a-4",
@@ -15,3 +15,37 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+const singup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email
+    });
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+}
+
+
+const login = async(email,password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+
+  } catch (error) {
+    console.log(error)
+    alert(error);
+  } 
+}
+
+
+const logout = () => {
+  signOut(auth);
+}
+
+export { auth, db, login, singup, logout };
